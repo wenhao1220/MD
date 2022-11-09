9ad338fe504c132c401fcbe8c6709e7a8007b24b
wenhao1220 <whlee1220@gmail,com>
2022-11-10 04:22:46 +0800

    first commit

---
+++ MID_test/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.困難度DToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.D20 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.D10 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.D5 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.困難度DToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(10, 3, 0, 3)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1260, 40)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '困難度DToolStripMenuItem
+        '
+        Me.困難度DToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.D20, Me.D10, Me.D5})
+        Me.困難度DToolStripMenuItem.Name = "困難度DToolStripMenuItem"
+        Me.困難度DToolStripMenuItem.Size = New System.Drawing.Size(139, 38)
+        Me.困難度DToolStripMenuItem.Text = "困難度(&D)"
+        '
+        'D20
+        '
+        Me.D20.Name = "D20"
+        Me.D20.Size = New System.Drawing.Size(359, 44)
+        Me.D20.Text = "高20x20(&H)"
+        '
+        'D10
+        '
+        Me.D10.Checked = True
+        Me.D10.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.D10.Name = "D10"
+        Me.D10.Size = New System.Drawing.Size(359, 44)
+        Me.D10.Text = "中10x10(&M)"
+        '
+        'D5
+        '
+        Me.D5.Name = "D5"
+        Me.D5.Size = New System.Drawing.Size(359, 44)
+        Me.D5.Text = "低5x5(&L)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1260, 893)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 困難度DToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents D20 As ToolStripMenuItem
+    Friend WithEvents D10 As ToolStripMenuItem
+    Friend WithEvents D5 As ToolStripMenuItem
+End Class
---
+++ MID_test/Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+    Dim WithEvents w_timer As New Timer
+    Dim w_tick As Double
+    Dim check As Integer = 1
+    Public Row = 3
+    Public Col = 3
+    Public Bo = (Row + 1) * (Col + 1)
+    Public BB = 30
+    Public BOT(Row, Col) As Button
+    Public Open As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Open = 0
+        Me.Width = 300
+        Me.Height = 300
+        For i = 0 To Row
+            For j = 0 To Col
+                BOT(i, j) = New Button
+                BOT(i, j).Refresh()
+                Me.Controls.Add(BOT(i, j))
+                BOT(i, j).Image = My.Resources.ResourceManager.GetObject("box")
+                BOT(i, j).Width = BB
+                BOT(i, j).Height = BB
+                BOT(i, j).BackgroundImageLayout = ImageLayout.Stretch
+                BOT(i, j).Top = 35 + i * BB
+                BOT(i, j).Left = 10 + j * BB
+                BOT(i, j).Tag = 0
+                AddHandler BOT(i, j).MouseUp, AddressOf A
+
+            Next
+        Next
+        Randomize()
+        Getnumber(Bo)
+    End Sub
+
+    Private Sub A(sender As Object, e As MouseEventArgs)
+        Dim B = CType(sender, Button)
+
+        If B.Tag = check Then
+            B.Image = Nothing
+            B.Text = B.Tag
+            B.Enabled = False
+            check += 1
+        Else
+            B.Image = Nothing
+            B.Enabled = False
+            B.Text = B.Tag
+            wait(1)
+            B.Text = Nothing
+            B.Image = CType(My.Resources.ResourceManager.GetObject("box"), Image)
+            B.Enabled = True
+        End If
+
+        If check - 1 = (Row + 1) * (Col + 1) Then
+            MsgBox("恭喜通關!是否開啟新局?")
+        End If
+
+    End Sub
+
+    Sub Getnumber(BoomNumber As Integer)
+        Dim r As New Random
+        Dim Number, No1, No2, temp1, temp2 As Integer
+        Dim n As Integer = 1
+
+        For i = 0 To Row
+            For j = 0 To Col
+                BOT(i, j).Tag = n
+                n += 1
+            Next
+        Next
+
+        For i = 0 To Row
+            For j = 0 To Col
+                Number = r.Next((Row + 1) * (Col + 1) - 1)
+                No1 = Number \ (Row + 1)
+                No2 = Number Mod (Col + 1)
+
+                temp1 = BOT(i, j).Tag
+                temp2 = BOT(No1, No2).Tag
+                BOT(i, j).Tag = temp2
+                BOT(No1, No2).Tag = temp1
+            Next
+        Next
+
+    End Sub
+
+    Private Sub wait(ByVal second As Integer)
+        w_tick = 0
+        w_timer.Interval = second * 1000
+        w_timer.Enabled = True
+        Do Until w_tick >= 1
+            Application.DoEvents()
+        Loop
+        w_timer.Enabled = False
+        w_timer.Interval = 1
+    End Sub
+
+    Private Sub w_timer_tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles w_timer.Tick
+        w_tick += 1
+    End Sub
+
+    Private Sub D20_Click_1(sender As Object, e As EventArgs) Handles D20.Click
+        D20.Checked = True
+        For i = 0 To Row
+            For j = 0 To Col
+                If Me.Contains(BOT(i, j)) Then
+                    Me.Controls.Remove(BOT(i, j))
+                    BOT(i, j).Refresh()
+                End If
+            Next
+        Next
+        Row = 2
+        Col = 2
+        Bo = 10
+        BB = 30
+        ReDim BOT(Row, Col)
+        Form1_Load(sender, e)
+    End Sub
+End Class
